fdfcf500bed37ca6b9808fcf064056ede6f256f4
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-07-27 20:58:21 +0200

    chore: slight cleanup

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -13,20 +13,5 @@ export async function load({ fetch, data }) {
 		schedule1: json1?.data ?? [],
 		schedule2: json2?.data ?? [],
 		favourites: data.favourites
-		// favourites: [
-		//   {
-		//     length: 3000,
-		//     scheduled: '2023-07-22T12:00:00Z',
-		//     game: '[Mega Man 2.5D](https://www.twitch.tv/videos/1878672037)',
-		//     players: ['[BigJon](https://twitch.tv/BigJon)'],
-		//     platform: 'PC',
-		//     category: 'Any% (Normal, Single run)',
-		//     note: null,
-		//     layout: '4x3-1p',
-		//     info: null,
-		//     id: '1b396ede66d84c85ab50b4c6ca959998',
-		//     options: null
-		//   }
-		// ]
 	};
 }
