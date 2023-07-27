ff8147b5041836e0f79fd0c570f8789d471f4d19
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-07-27 20:56:58 +0200

    feat + chore: add duration, remove svelte-markdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,115 @@
 <script>
-	import SvelteMarkdown from 'svelte-markdown';
+	import { enhance } from '$app/forms';
+	import moment from 'moment';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	// const find = (id) => data.schedule.find((s) => s.id === id);
 	$: favs = data.favourites
 		.map((f) => [...data.schedule1, ...data.schedule2].find((s) => s.id === f.id))
 		.sort((a, b) => new Date(a.scheduled).getTime() - new Date(b.scheduled).getTime());
-	//$: console.log('favs', favs);
+
+	/**
+	 * @param {string} markdownLink
+	 * @returns {Array <string|undefined>}
+	 */
+	const getLink = (markdownLink) => {
+		const regex = /\[(.*)\]\((.*)\)/;
+		if (regex.test(markdownLink)) {
+			const match = regex.exec(markdownLink);
+			if (match && match[1] && match[2]) {
+				return [match[1], match[2]];
+			}
+		}
+		return [markdownLink, undefined];
+	};
 </script>
 
 <h1>Favourites</h1>
 {#each favs as f (f.id)}
-	<span>{new Date(f.scheduled).toString()}</span>
-	<form method="POST" action="?/remove">
-		<input type="hidden" name="id" value={f.id} />
-		<button type="submit">Remove</button>
-	</form>
-	<SvelteMarkdown source={f.game} />
-	{#each f.players as p}
-		<SvelteMarkdown source={p} />
-	{/each}
+	<div class="schedule-item">
+		<span>{moment(f.scheduled).format('dddd, MMMM Do HH:mm')}</span>
+		<span>{moment.utc(moment.duration(f.length, 'seconds').asMilliseconds()).format('HH:mm')}</span>
+		{#if getLink(f.game)[1]}
+			<a href={getLink(f.game)[1]}>{getLink(f.game)[0]}</a>
+		{:else}
+			<span>{getLink(f.game)[0]}</span>
+		{/if}
+		<div class="players">
+			{#each f.players as p}
+				{#if getLink(p)[1]}
+					<a href={getLink(p)[1]}>{getLink(p)[0]}</a>
+				{:else}
+					<span>{getLink(p)[0]}</span>
+				{/if}
+			{/each}
+		</div>
+		<form method="POST" action="?/remove" use:enhance>
+			<input type="hidden" name="id" value={f.id} />
+			<button type="submit">Remove</button>
+		</form>
+	</div>
 {/each}
 <h1>Stream 1</h1>
 {#each data.schedule1 as s (s.id)}
-	<span>{new Date(s.scheduled).toString()}</span>
-	<form method="POST" action="?/add">
-		<input type="hidden" name="id" value={s.id} />
-		<button type="submit">Add</button>
-	</form>
-	<SvelteMarkdown source={s.game} />
-	{#each s.players as p}
-		<SvelteMarkdown source={p} />
-	{/each}
+	<div class="schedule-item">
+		<span>{moment(s.scheduled).format('dddd, MMMM Do HH:mm')}</span>
+		{#if getLink(s.game)[1]}
+			<a href={getLink(s.game)[1]}>{getLink(s.game)[0]}</a>
+		{:else}
+			<span>{getLink(s.game)[0]}</span>
+		{/if}
+		<div class="players">
+			{#each s.players as p}
+				{#if getLink(p)[1]}
+					<a href={getLink(p)[1]}>{getLink(p)[0]}</a>
+				{:else}
+					<span>{getLink(p)[0]}</span>
+				{/if}
+			{/each}
+		</div>
+		<form method="POST" action="?/add" use:enhance>
+			<input type="hidden" name="id" value={s.id} />
+			<button type="submit">Add</button>
+		</form>
+	</div>
 {/each}
 <h1>Stream 2</h1>
 {#each data.schedule2 as s (s.id)}
-	<span>{new Date(s.scheduled).toTimeString()}</span>
-	<form method="POST" action="?/add">
-		<input type="hidden" name="id" value={s.id} />
-		<button type="submit">Add</button>
-	</form>
-	<SvelteMarkdown source={s.game} />
-	{#each s.players as p}
-		<SvelteMarkdown source={p} />
-	{/each}
+	<div class="schedule-item">
+		<span>{moment(s.scheduled).format('dddd, MMMM Do HH:mm')}</span>
+		{#if getLink(s.game)[1]}
+			<a href={getLink(s.game)[1]}>{getLink(s.game)[0]}</a>
+		{:else}
+			<span>{getLink(s.game)[0]}</span>
+		{/if}
+		<div class="players">
+			{#each s.players as p}
+				{#if getLink(p)[1]}
+					<a href={getLink(p)[1]}>{getLink(p)[0]}</a>
+				{:else}
+					<span>{getLink(p)[0]}</span>
+				{/if}
+			{/each}
+		</div>
+		<form method="POST" action="?/add" use:enhance>
+			<input type="hidden" name="id" value={s.id} />
+			<button type="submit">Add</button>
+		</form>
+	</div>
 {/each}
+
+<style>
+	.schedule-item {
+		margin-bottom: 1rem;
+		display: flex;
+		align-items: center;
+	}
+	.players {
+		display: flex;
+		margin: 0.5rem;
+	}
+	span {
+		margin: 0.5rem;
+	}
+</style>
