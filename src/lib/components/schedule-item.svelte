924d88e5762226ef81a5c4c3fc1c273abe42ad0b
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2023-07-28 22:10:17 +0200

    chore: move schedule item to own component

---
+++ src/lib/components/schedule-item.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { enhance } from '$app/forms';
+	import moment from 'moment';
+
+	/** @type {import('$lib/types').ScheduleItem} */
+	export let scheduleItem;
+
+	$: gameLink = getLink(scheduleItem.game);
+	$: playerLink = getLink(scheduleItem.players[0]);
+
+	/**
+	 * @param {string} markdownLink
+	 * @returns {Array <string|undefined>}
+	 */
+	const getLink = (markdownLink) => {
+		const regex = /\[(.*)\]\((.*)\)/;
+		if (regex.test(markdownLink)) {
+			const match = regex.exec(markdownLink);
+			if (match && match[1] && match[2]) {
+				return [match[1], match[2]];
+			}
+		}
+		return [markdownLink, undefined];
+	};
+</script>
+
+<div class="schedule-item" transition:fade>
+	<span>{moment(scheduleItem.scheduled).format('dddd, MMMM Do HH:mm')}</span>
+	<span
+		>{moment
+			.utc(moment.duration(scheduleItem.length, 'seconds').asMilliseconds())
+			.format('HH:mm')}</span
+	>
+	{#if gameLink[1]}
+		<a href={gameLink[1]}>{gameLink[0]}</a>
+	{:else}
+		<span>{gameLink[0]}</span>
+	{/if}
+	<div class="players">
+		<!-- {#each scheduleItem.players as p} -->
+		<div class="player">
+			{#if playerLink[1]}
+				<a href={playerLink[1]}>{playerLink[0]}</a>
+			{:else}
+				<span>{playerLink[0]}</span>
+			{/if}
+		</div>
+		<!-- {/each} -->
+	</div>
+	<span class="category">{scheduleItem.category}</span>
+	<form method="POST" action="?/remove" use:enhance>
+		<input type="hidden" name="id" value={scheduleItem.id} />
+		<button type="submit">Remove</button>
+	</form>
+</div>
+
+<style>
+	.schedule-item {
+		margin-bottom: 1rem;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import { fade } from 'svelte/transition';
-	import { enhance } from '$app/forms';
-	import moment from 'moment';
+	import ScheduleItem from '$lib/components/schedule-item.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -22,133 +20,24 @@
 		/** @param {import('$lib/types').ScheduleItem} s */
 		(s) => JSON.stringify(s).toLowerCase().includes(filter.toLowerCase())
 	);
-
-	/**
-	 * @param {string} markdownLink
-	 * @returns {Array <string|undefined>}
-	 */
-	const getLink = (markdownLink) => {
-		const regex = /\[(.*)\]\((.*)\)/;
-		if (regex.test(markdownLink)) {
-			const match = regex.exec(markdownLink);
-			if (match && match[1] && match[2]) {
-				return [match[1], match[2]];
-			}
-		}
-		return [markdownLink, undefined];
-	};
 </script>
 
 <input type="text" bind:value={filter} />
 <h1>Favourites</h1>
 <div class="schedule-wrapper">
-	{#each filteredFavs as s (s.id)}
-		{@const link = getLink(s.players[0])}
-		<div class="schedule-item" transition:fade>
-			<span>{moment(s.scheduled).format('dddd, MMMM Do HH:mm')}</span>
-			<span
-				>{moment.utc(moment.duration(s.length, 'seconds').asMilliseconds()).format('HH:mm')}</span
-			>
-			{#if getLink(s.game)[1]}
-				<a href={getLink(s.game)[1]}>{getLink(s.game)[0]}</a>
-			{:else}
-				<span>{getLink(s.game)[0]}</span>
-			{/if}
-			<div class="players">
-				<!-- {#each s.players as p} -->
-				<div class="player">
-					{#if link[1]}
-						<a href={link[1]}>{link[0]}</a>
-					{:else}
-						<span>{link[0]}</span>
-					{/if}
-				</div>
-				<!-- {/each} -->
-			</div>
-			<span class="category">{s.category}</span>
-			<form method="POST" action="?/remove" use:enhance>
-				<input type="hidden" name="id" value={s.id} />
-				<button type="submit">Remove</button>
-			</form>
-		</div>
+	{#each filteredFavs as scheduleItem (scheduleItem.id)}
+		<ScheduleItem {scheduleItem} />
 	{/each}
 </div>
 <h1>Stream 1</h1>
 <div class="schedule-wrapper">
-	{#each filteredStream1 as s (s.id)}
-		{@const link = getLink(s.players[0])}
-		<div class="schedule-item">
-			<span>{moment(s.scheduled).format('dddd, MMMM Do HH:mm')}</span>
-			<span
-				>{moment.utc(moment.duration(s.length, 'seconds').asMilliseconds()).format('HH:mm')}</span
-			>
-			{#if getLink(s.game)[1]}
-				<a href={getLink(s.game)[1]}>{getLink(s.game)[0]}</a>
-			{:else}
-				<span>{getLink(s.game)[0]}</span>
-			{/if}
-			<div class="players">
-				<!-- {#each s.players as p} -->
-				<div class="player">
-					{#if link[1]}
-						<a href={link[1]}>{link[0]}</a>
-					{:else}
-						<span>{link[0]}</span>
-					{/if}
-				</div>
-				<!-- {/each} -->
-			</div>
-			<span class="category">{s.category}</span>
-			<form method="POST" action="?/add" use:enhance>
-				<input type="hidden" name="id" value={s.id} />
-				<button type="submit">Add</button>
-			</form>
-		</div>
+	{#each filteredStream1 as scheduleItem (scheduleItem.id)}
+		<ScheduleItem {scheduleItem} />
 	{/each}
 </div>
 <h1>Stream 2</h1>
 <div class="schedule-wrapper">
-	{#each filteredStream2 as s (s.id)}
-		{@const link = getLink(s.players[0])}
-		<div class="schedule-item">
-			<span>{moment(s.scheduled).format('dddd, MMMM Do HH:mm')}</span>
-			<span
-				>{moment.utc(moment.duration(s.length, 'seconds').asMilliseconds()).format('HH:mm')}</span
-			>
-			{#if getLink(s.game)[1]}
-				<a href={getLink(s.game)[1]}>{getLink(s.game)[0]}</a>
-			{:else}
-				<span>{getLink(s.game)[0]}</span>
-			{/if}
-			<div class="players">
-				<!-- {#each s.players as p} -->
-				<div class="player">
-					{#if link[1]}
-						<a href={link[1]}>{link[0]}</a>
-					{:else}
-						<span>{link[0]}</span>
-					{/if}
-				</div>
-				<!-- {/each} -->
-			</div>
-			<span class="category">{s.category}</span>
-			<form method="POST" action="?/add" use:enhance>
-				<input type="hidden" name="id" value={s.id} />
-				<button type="submit">Add</button>
-			</form>
-		</div>
+	{#each filteredStream2 as scheduleItem (scheduleItem.id)}
+		<ScheduleItem {scheduleItem} />
 	{/each}
 </div>
-
-<style>
-	.schedule-item {
-		margin-bottom: 1rem;
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-	}
-	.players {
-		display: flex;
-	}
-	span {
-	}
-</style>
